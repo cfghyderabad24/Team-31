5fad97c1418cfce40fc7ad22fb6dcc3148244c71
Pavan17-cpu <110300098+Pavan17-cpu@users.noreply.github.com>
2024-06-23 02:08:46 +0530

    Update booksRoute.js(Pavan)

--- server/routes/booksRoute.js
+++ server/routes/booksRoute.js
@@ -1,147 +1,54 @@
-const express = require("express");
-const router = express.Router();
-const User = require("../models/usersModel");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+const router = require("express").Router();
+const Book = require("../models/booksModel");
 const authMiddleware = require("../middlewares/authMiddleware");
 
-// register a new user
-router.post("/register", async (req, res) => {
+// add a book
+router.post("/add-book", authMiddleware, async (req, res) => {
   try {
-    // check if user already exists
-    const user = await User.findOne({ email: req.body.email });
-    if (user) {
-      return res.send({
-        success: false,
-        message: "Email already exists",
-      });
-    }
-
-    // hash password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
-    req.body.password = hashedPassword;
-
-    // create new user
-    const newUser = new User(req.body);
-    await newUser.save();
-    return res.send({
-      success: true,
-      message: "User created successfully , please login",
-    });
+    const newBook = new Book(req.body);
+    await newBook.save();
+    return res.send({ success: true, message: "Book added successfully" });
   } catch (error) {
-    return res.send({
-      success: false,
-      message: error.message,
-    });
+    return res.send({ success: false, message: error.message });
   }
 });
 
-// login a user
-router.post("/login", async (req, res) => {
+// update a book
+router.put("/update-book/:id", authMiddleware, async (req, res) => {
   try {
-    // check if user exists
-    const user = await User.findOne({ email: req.body.email });
-    if (!user) {
-      return res.send({
-        success: false,
-        message: "User does not exist",
-      });
-    }
-
-    // check if password is correct
-    const validPassword = await bcrypt.compare(
-      req.body.password,
-      user.password
-    );
-
-    if (!validPassword) {
-      return res.send({
-        success: false,
-        message: "Invalid password",
-      });
-    }
-
-    // create and assign a token
-    const token = jwt.sign({ userId: user._id }, process.env.jwt_secret, {
-      expiresIn: "1d",
-    });
-    return res.send({
-      success: true,
-      message: "Login successful",
-      data: token,
-    });
+    await Book.findByIdAndUpdate(req.params.id, req.body);
+    return res.send({ success: true, message: "Book updated successfully" });
   } catch (error) {
-    return res.send({
-      success: false,
-      message: error.message,
-    });
+    return res.send({ success: false, message: error.message });
   }
 });
 
-// get logged in user details
-router.get("/get-logged-in-user", authMiddleware, async (req, res) => {
+// delete a book
+router.delete("/delete-book/:id", authMiddleware, async (req, res) => {
   try {
-    const user = await User.findById(req.body.userIdFromToken);
-    if (!user) {
-      return res.send({
-        success: false,
-        message: "User does not exist",
-      });
-    }
-    return res.send({
-      success: true,
-      message: "User details fetched successfully",
-      data: user,
-    });
+    await Book.findByIdAndDelete(req.params.id);
+    return res.send({ success: true, message: "Book deleted successfully" });
   } catch (error) {
-    return res.send({
-      success: false,
-      message: error.message,
-    });
+    return res.send({ success: false, message: error.message });
   }
 });
 
-// get all the users (patrons)
-router.get("/get-all-users/:role", authMiddleware, async (req, res) => {
+// get all books
+router.get("/get-all-books", authMiddleware, async (req, res) => {
   try {
-    const users = await User.find({ role: req.params.role }).sort({
-      createdAt: -1,
-    });
-    return res.send({
-      success: true,
-      message: "Users fetched successfully",
-      data: users,
-    });
+    const books = await Book.find().sort({ createdAt: -1 });
+    return res.send({ success: true, data: books });
   } catch (error) {
-    return res.send({
-      success: false,
-      message: error.message,
-    });
+    return res.send({ success: false, message: error.message });
   }
 });
-
-// get user by id
-router.get("/get-user-by-id/:id", authMiddleware, async (req, res) => {
+// get a book by id
+router.get("/get-book-by-id/:id", authMiddleware, async (req, res) => {
   try {
-    const user = await User.findById(req.params.id);
-    if (!user) {
-      return res.send({
-        success: false,
-        message: "User does not exist",
-      });
-    }
-    return res.send({
-      success: true,
-      message: "User fetched successfully",
-      data: user,
-    });
-
+    const book = await Book.findById(req.params.id);
+    return res.send({ success: true, data: book });
   } catch (error) {
-    return res.send({
-      success: false,
-      message: 'User does not exist',
-    });
+    return res.send({ success: false, message: error.message });
   }
 });
 
